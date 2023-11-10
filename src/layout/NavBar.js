179d8b440a928366d0bd3f43d09da0059f69a049
Ian Kimani <ianmwng@gmail.com>
2023-11-10 18:05:49 +0300

    made changes

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Image from '../images/comic.jpeg';
+import Image from '../images/comic.png';
 
 export default function Navbar(props) {
   return (
