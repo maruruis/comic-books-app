ce0c9d9bdcc9d7c86354ff7de2775f20ad7fed3f
Ian Kimani <ianmwng@gmail.com>
2023-11-10 10:55:16 +0300

    made changes on the code

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import img from '../images/comic.jpeg';
 export default function Navbar(props) 
 {
   return (
     <div>
     
     <nav className="navbar navbar-expand-lg  bg-white ">
-  <div className="container-fluid">
-    
+    <div className="container-fluid">
+    <a className="navbar-brand" href="/">Comic book</a>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <Link className="nav-link active" to="/">Home</Link>
