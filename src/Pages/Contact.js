1e66b8eff2c450351145d1039a51a6e84d441205
Ian Kimani <ianmwng@gmail.com>
2023-12-24 22:39:58 +0300

    changes on contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
       <h1 className='text-center mt-4'>About</h1>
       <p className='text-center'>
       We are always happy to hear from you. If you have any questions or comments, please feel free to contact us using the information below.<br/>
-      Telephone:0797050698<br/>Email Adress:ianmwng@gmail.com<br/>Adress: 1034-0100, thika<br/>City: Nirobi, kenya
+      Telephone:0797050698<br/>Email Adress:ianmwng@gmail.com<br/>Adress: 1034-0100, thika<br/>City: Nairobi, kenya
 
       </p>
       
